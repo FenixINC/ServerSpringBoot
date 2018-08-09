bcf700d9865c1789bc2b1c817f9ad4b5cd886238
Taras Koloshmatin <taras@pier2marketing.com>
2018-08-09 15:24:11 +0300

    fix; changed port number

--- src/main/java/com/server/server/ServerApplication.java
+++ src/main/java/com/server/server/ServerApplication.java
@@ -1,13 +1,18 @@
 package com.server.server;
 
+import org.jboss.jandex.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Main.class);
+        application.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+
+        application.run(args);
+    }
 }
