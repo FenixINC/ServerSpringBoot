b895e3430b836123c454f030d80a1334aa311e9e
Taras Koloshmatin <taras@pier2marketing.com>
2018-08-10 10:34:22 +0300

    fix! - pom.xml

--- src/main/java/com/server/server/Application.java
+++ src/main/java/com/server/server/Application.java
@@ -1,18 +1,21 @@
 package com.server.server;
 
-import org.jboss.jandex.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Collections;
 
+/**
+ * Created by Taras Koloshmatin on 09.08.2018
+ */
 @SpringBootApplication
-public class ServerApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Main.class);
+        SpringApplication application = new SpringApplication(Application.class);
         application.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-
         application.run(args);
+
+//        SpringApplication.run(Application.class, args);
     }
 }
+
