5101b8a6307a16e76b1ebfbc263a5ca8c848795e
Taras Koloshmatin <taras@pier2marketing.com>
2018-12-11 17:03:24 +0200

    implemented simple login/home web pages

--- src/main/java/com/server/server/config/WebConfig.java
+++ src/main/java/com/server/server/config/WebConfig.java
@@ -7,7 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,14 +16,20 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.server.server")
-public class WebConfig extends WebMvcConfigurerAdapter {
-                    // implements WebMvcConfigurer
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(new ObjectMapper());
         converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
-
         converters.add(converter);
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
     }
 }
